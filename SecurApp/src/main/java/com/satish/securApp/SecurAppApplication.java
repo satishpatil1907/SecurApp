6cecfb109d4ce119e0480d9a220c0b13d2388998
satish <patil10c@gmail.com>
2019-11-25 12:03:25 +0530

    commit all files

---
+++ SecurApp/src/main/java/com/satish/securApp/SecurAppApplication.java
@@ -0,0 +1,13 @@
+package com.satish.securApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurAppApplication.class, args);
+	}
+
+}
